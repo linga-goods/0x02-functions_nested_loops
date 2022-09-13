62f69dd56446c53995c02da21abd114f4ed21590
Anthony Banda <lingaanthonybanda@gmail.com>
2022-09-13 05:51:09 -0700

    Adding main.c to the repository

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main-the starting point of code
+ *
+ * Return: 0 for success
+ */
+int main()
+{
+	_putchar("_putchar");
+
+	_putchar('\n');
+
+	return (0);
+
+}
